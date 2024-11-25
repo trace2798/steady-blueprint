ceb14820e8069cdd1fff647610ba27b8905a5056
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-11-25 19:52:02 +0530

    added function to get quote

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,29 @@
+import { http } from "@hypermode/modus-sdk-as";
+
 export function sayHello(name: string | null = null): string {
   return `Hello, ${name || "World"}!`;
+}
+
+@json
+class Quote {
+
+  @alias("q")
+  quote!: string;
+
+
+  @alias("a")
+  author!: string;
+}
+
+export function getRandomQuote(): Quote {
+  const request = new http.Request("https://zenquotes.io/api/random");
+
+  const response = http.fetch(request);
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch quote. Received: ${response.status} ${response.statusText}`,
+    );
+  }
+
+  return response.json<Quote[]>()[0];
 }
